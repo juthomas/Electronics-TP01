6564dc2971e107588f5770f7ab3de642172849a3
labelec <labelec@e1r1p3.42.fr>
2020-10-29 18:05:30 +0100

    [ADD] links + beg

--- ex00/main.c
+++ ex00/main.c
@@ -1,4 +1,16 @@
+#include <avr/io.h>
+
 int main()
 {
-	return (0);
+	DDRB |= 1 << 1;
+	
+	TCCR1A = 0;
+  	TCCR1B = 0;
+  	TCCR1B |= CS12;  // 256 prescaler
+  	OCR1A = 62500;
+  	
+	
+	TCCR1A |= COM1A0; // Toggle pin OC1A (9)
+	TCCR1A |= COM1A1; // Toggle pin OC1A (9)
+	for(;;);
 }
