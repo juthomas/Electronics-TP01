617ccc2b90e922a1de996a6cd96a3b5a8b525502
The Drunken Wizard <johangourdin94@gmail.com>
2020-11-01 00:19:01 +0100

    aaaaa

--- ex00/main.c
+++ ex00/main.c
@@ -6,8 +6,8 @@ int main()
 	
 	TCCR1A = 0;
   	TCCR1B = 0;
-  	TCCR1B |= 1 << CS12;  // 256 * 62500 = 16 000 000 soit 1sec vu que le quartz est a 16Mhz
-  	OCR1A = 62500; // Here we tell that whenever Timer1 gets to 62500 it does something
+  	TCCR1B |= 1 << CS12; // 256 * 31250 = 8 000 000 soit 0.5sec vu que le quartz est a 16Mhz
+  	OCR1A = 31250; // Here we tell that whenever Timer1 gets to 62500 it does something
   	
 	
 	TCCR1A |= 1 << COM1A0; // Here we tell that it have to be in Compare Match and toogle 0C1A (our LED).
