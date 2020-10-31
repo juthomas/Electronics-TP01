6d5a481cb41145b5aa0786e371456aca178d6903
The Drunken Wizard <johangourdin94@gmail.com>
2020-11-01 00:15:36 +0100

    aaaaa

--- ex00/main.c
+++ ex00/main.c
@@ -6,14 +6,11 @@ int main()
 	
 	TCCR1A = 0;
   	TCCR1B = 0;
-  	TCCR1B |= 1 << CS12;  // 256 prescaler
-  	OCR1A = 62500;
+  	TCCR1B |= 1 << CS12;  // 256 * 62500 = 16 000 000 soit 1sec vu que le quartz est a 16Mhz
+  	OCR1A = 62500; // Here we tell that whenever Timer1 gets to 62500 it does something
   	
 	
-	TCCR1A |= 1 << COM1A0; // Toggle pin OC1A (9)
-	//TCCR1A |= COM1A1; // Toggle pin OC1A (9)
-
-	//TCCR1B = (1 << CS11);  //Prescale clock by 8
+	TCCR1A |= 1 << COM1A0; // Here we tell that it have to be in Compare Match and toogle 0C1A (our LED).
 
 	for(;;);
 }
