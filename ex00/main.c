dcf3c9070274095d3d1a3adfac99925492c2f44c
BuildTools <unconfigured@null.spigotmc.org>
2020-11-01 00:08:32 +0100

    [ADD] working

--- ex00/main.c
+++ ex00/main.c
@@ -1,16 +1,15 @@
+#ifndef __AVR_ATmega328P__
+# define __AVR_ATmega328P__
+#endif
 #include <avr/io.h>
 
 int main()
 {
-	DDRB |= 1 << 1;
-	
-	TCCR1A = 0;
-  	TCCR1B = 0;
-  	TCCR1B |= CS12;  // 256 prescaler
-  	OCR1A = 62500;
-  	
-	
-	TCCR1A |= COM1A0; // Toggle pin OC1A (9)
-	TCCR1A |= COM1A1; // Toggle pin OC1A (9)
-	for(;;);
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCCR1B |= 1 << CS12;
+  OCR1A = 62500;
+  TCCR1A |= 1 << COM1A0;
+	DDRB = 1 << 1;
+  for(;;);
 }
