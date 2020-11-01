3dbcd0479f28fcf74adf50ccb7521a05f219b4a5
BuildTools <unconfigured@null.spigotmc.org>
2020-11-01 22:21:18 +0100

    [ADD] comments

--- ex01/main.c
+++ ex01/main.c
@@ -5,11 +5,10 @@
 
 int main()
 {
-  // pinMode(9, OUTPUT);                               // Set digital pin 9 (D9) to an output
-  DDRB |= 1 << 1;
-  TCCR1A = (1 << COM1A1) |  (1 << WGM11);                // Enable the PWM output OC1A on digital pins 9
-  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS12);     // Set fast PWM and prescaler of 256 on timer 1
-  ICR1 = 62500;                                     // Set the PWM frequency to 1Hz: 16MHz/(256 * 1Hz) - 1 = 62499
-  OCR1A = 6250;                                     // Set the duty-cycle to 10%: 62499
+  DDRB |= 1 << 1;                                       // (OC1A PB1) on met OC1A en OUTPUT
+  TCCR1A = (1 << COM1A1) |  (1 << WGM11);                // On active le mode PWM sur OC1A (16.11.1)
+  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS12);     // On active fast PWM et le prescaler sur 256 pour le timer 1 (16.11.2)
+  ICR1 = 62500;                                     // On met la frequence PWM sur 1Hz: 16MHz/(256 * 1Hz) = 62500
+  OCR1A = 6250;                                     // On met le duty-cycle sur 10%: 62500 * 0.1 = 6250
   for(;;);
 }
