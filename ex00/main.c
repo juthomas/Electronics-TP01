83b9bec16d747a5395c043909e328855f742019e
BuildTools <unconfigured@null.spigotmc.org>
2020-11-01 23:18:43 +0100

    [ADD] Timer 32250 -> 31250

--- ex00/main.c
+++ ex00/main.c
@@ -7,8 +7,8 @@ int main()
 {
 	// On met OC1A (PB1) en OUTPUT
 	DDRB |= 1 << 1;
-	// On met le timer sur 500ms: 16MHz/(256 * 0.5) = 32250
-	OCR1A = 32250;
+	// On met le timer sur 500ms: 16MHz/(256 * 0.5) = 31250
+	OCR1A = 31250;
 	// On met le prescaler sur 256 pour le timer 1 (16.11.2)
 	TCCR1B = (1 << WGM12) |(1 << CS12);
 	// On Toggle OC1A a chaque fois que le timer a match (Table 16-1)
