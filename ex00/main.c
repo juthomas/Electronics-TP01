7a7bb5be09faedf6a4572b86349f2df34c1e1dce
The Drunken Wizard <johangourdin94@gmail.com>
2020-11-01 21:05:51 +0100

    ad

--- ex00/main.c
+++ ex00/main.c
@@ -3,14 +3,13 @@
 int main()
 {
 	DDRB |= 1 << 1;
-	
 	TCCR1A = 0;
   	TCCR1B = 0;
-  	TCCR1B |= 1 << CS12; // 256 * 31250 = 8 000 000 soit 0.5sec vu que le quartz est a 16Mhz
-  	OCR1A = 31250; // Here we tell that whenever Timer1 gets to 62500 it does something
+	OCR1A = 32550;
+  	TCCR1B |= (1<<WGM12)|(1<<CS12); // We get the CTC Mode that clear the timer on compare match
   	
-	
-	TCCR1A |= 1 << COM1A0; // Here we tell that it have to be in Compare Match and toogle 0C1A (our LED).
+	TCCR1A |= 1 << COM1A0;
+	 // Here we tell that it have to be in Compare Match and toogle 0C1A (our LED).
 
 	for(;;);
 }
